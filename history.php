d2f2bcc0531d5d8796efe36abfd7a271c8089e33
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-03-31 19:11:17 +0600

    * Fixed query for multiple user (role based queryset) * Added history for card blocking

--- history.php
+++ history.php
@@ -28,11 +28,12 @@
         include 'helper/navbar.html';
     ?>
 
+    <!-- Transaction history -->
     <div class="row">
         <div class="col-sm-12 col-md-12">
             <div class="panel panel-success">
                 <div class="panel-heading">
-                    <p class="text-22px text-center">Settings</p>
+                    <p class="text-22px text-center">Transaction History</p>
                 </div>
                 <div class="panel-body">
                     <table class="table">
@@ -43,22 +44,27 @@
                             if($user_data->num_rows == 1){
                                 $user_pk = $user_data->fetch_row()[0];
                             }
-                            $account_pk = $_SESSION['account_id'];
+                            $get_account_sql = "SELECT id from account WHERE user_id=".$user_pk;
+                            $account_data = $conn->query($get_account_sql);
+                            if($account_data->num_rows == 1) {
+                                $account_pk = $account_data->fetch_row()[0];
+                            }
 
-                            $sql = "SELECT transaction.account_id, transaction.amount, transaction.branch_id, created_at, account.id, account.user_id, users.id, users.name AS user_name, branch.id, branch.name AS branch_name FROM transaction, account, users, branch WHERE transaction.account_id=account.id AND account.user_id=users.id AND branch.id=transaction.branch_id";
+                            $sql = "SELECT transaction.account_id, transaction.amount, transaction.branch_id, created_at, account.id, account.user_id, users.id, users.name AS user_name, branch.id, branch.name AS branch_name FROM transaction, account, users, branch WHERE transaction.account_id=".$account_pk." AND account.user_id=".$user_pk." AND branch.id=transaction.branch_id AND users.id=".$user_pk." ORDER BY transaction.id";
                              
                             $transaction_data = $conn->query($sql);
-                            echo '
-                                <thead>
-                                    <tr>
-                                        <th>User Name</th>
-                                        <th>Branch Name</th>
-                                        <th>Amount</th>
-                                        <th>Transaction Date & Time</th>
-                                    </tr>
-                                </thead>
-                            ';
+                            
                             if($transaction_data->num_rows > 0) {
+                                echo '
+                                    <thead>
+                                        <tr>
+                                            <th>User Name</th>
+                                            <th>Branch Name</th>
+                                            <th>Amount</th>
+                                            <th>Transaction Date & Time</th>
+                                        </tr>
+                                    </thead>
+                                ';
                                 while($row = $transaction_data->fetch_assoc()){
                                     echo '
                                         <tbody>
@@ -66,6 +72,64 @@
                                                 <td>'.$row["user_name"].'</td>
                                                 <td>'.$row["branch_name"].'</td>
                                                 <td>'.$row["amount"].'</td>
+                                                <td>'.$row["created_at"].'</td>
+                                            </tr>
+                                        </tbody>
+                                    ';
+                                }
+                            }else{
+                                echo '<p class="text-center">No data to show</p>';
+                            }
+                        ?>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Blocking History -->
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <div class="panel panel-success">
+                <div class="panel-heading">
+                    <p class="text-22px text-center">Blocking History</p>
+                </div>
+                <div class="panel-body">
+                    <table class="table">
+                        <?php
+                            $email = $_SESSION['username'];
+                            $user_data_sql = "SELECT id FROM users WHERE email='".$email."'";
+                            $user_data = $conn->query($user_data_sql);
+                            if($user_data->num_rows == 1){
+                                $user_pk = $user_data->fetch_row()[0];
+                            }
+                            
+                            $get_account_sql = "SELECT id from account WHERE user_id=".$user_pk;
+                            $account_data = $conn->query($get_account_sql);
+                            if($account_data->num_rows == 1) {
+                                $account_pk = $account_data->fetch_row()[0];
+                            }
+
+                            $sql = "SELECT block_history.account_id, block_history.branch_id, created_at, account.user_id, users.id, users.name AS user_name, branch.id, branch.name AS branch_name FROM block_history, account, users, branch WHERE block_history.account_id=".$account_pk." AND block_history.branch_id=branch.id AND account.user_id=".$user_pk." AND users.id=".$user_pk." ORDER BY created_at";
+                             
+                            $blocking_history = $conn->query($sql);
+                        
+                            if($blocking_history->num_rows > 0) {
+                                echo '
+                                    <thead>
+                                        <tr>
+                                            <th>Branch Name</th>
+                                            <th>Account User Name</th>
+                                            <th>Date & Time</th>
+                                        </tr>
+                                    </thead>
+                                ';
+                                while($row = $blocking_history->fetch_assoc()){
+                                    echo '
+                                        <tbody>
+                                            <tr>
+                                                <td>'.$row["branch_name"].'</td>
+                                                <td>'.$row["user_name"].'</td>
                                                 <td>'.$row["created_at"].'</td>
                                             </tr>
                                         </tbody>
